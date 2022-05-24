8204f823a92b37ae62f1f9e5e095f14c0cd9cc48
JaafarJawadi29 <91067539+JaafarJawadi29@users.noreply.github.com>
2022-05-24 16:44:54 +0200

    calendar testing

--- App/screens/CalendarScreen.js
+++ App/screens/CalendarScreen.js
@@ -1,10 +1,89 @@
 import { StyleSheet } from 'react-native';
 import { Text, View } from '../components/Themed';
 import React, { Component } from 'react';
-import BigCalendar from 'react-big-calendar';
-import moment from 'moment';
+import { month } from 'react-big-calendar/lib/utils/dates';
 
-export default function 
+var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+function drawCalendarMonths(){
+    for(var i = 0; i < months.length; i++){
+        var doc = document.createElement("div");
+        doc.innerHTML = months[i];
+        doc.classList.add("dropdown-item");
+
+        doc.onclick = (function () {
+            var selectedMonth = i;
+            return function ()
+            {
+                month = selectedMonth;
+                document.getElementById("curMonth").innerHTML = months[month];
+                loadCalendarDays();
+                return month;
+            }
+        })();
+        document.getElementById("months").appendChild(doc);
+    }
+}
+
+function loadYears(){
+    var startYear = 1900;
+    var endYear = 2022;
+
+    for(var i = startYear; i <= endYear; i++){
+        var doc = document.createElement("div");
+        doc.innerHTML = i;
+        doc.classList.add("dropdown-item");
+
+        doc.onclick = (function(){
+            var selectedYear = i;
+            return function(){
+                year = selectedYear;
+                document.getElementById("curYear").innerHTML = year;
+                loadCalendarDays();
+                return year;
+            }
+        })();
+        document.getElementById("years").appendChild(doc);
+    }
+}
+
+function daysInMonth(month, year){
+    let d = new Date(year, month+1, 0);
+    return d.getDate();
+}
+
+function loadCalendarDays(){
+    document.getElementById("calendarDays").innerHTML = "";
+
+    var tmpDate = new Date(year, month, 0);
+    var num = daysInMonth(month, year);
+    var dayofweek = tmpDate.getDay();
+
+    for(var i = 0; i <= dayofweek; i++){
+        var d = document.createElement("div");
+        d.classList.add("day");
+        d.classList.add("blank");
+        document.getElementById("calendarDays").appendChild(d);
+    }
+
+    for(var i = 0; i < num; i++){
+        var tmp = i + 1;
+        var d = document.createElement("div");
+        d.id = "calendarday_" + tmp;
+        d.className = "day";
+        d.innerHTML = tmp;
+        document.getElementById("calendarDays").appendChild(d);
+    }
+
+    var clear = document.createElement("div");
+    clear.className = "clear";
+    document.getElementById("calendarDays").appendChild(clear);
+}
+
+var selectedDays = new Array();
+var mousedown = false;
+
+
 
 const styles = StyleSheet.create({
   container: {
