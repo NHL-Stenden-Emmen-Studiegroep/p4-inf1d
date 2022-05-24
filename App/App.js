e60c77d7a5be0b063f5ee37603a3376784ebab41
Cedric Smit <cedsmit@gmail.com>
2022-05-24 09:37:39 +0200

    Migrated from typescript to Javascript

--- App/App.js
+++ App/App.js
@@ -1,8 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { useColorScheme } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
 export default function App() {
