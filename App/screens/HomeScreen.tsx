ff474c0bd9fffbb31f4c7869662f2b1bcad80099
JaafarJawadi29 <91067539+JaafarJawadi29@users.noreply.github.com>
2022-05-24 09:41:27 +0200

    comit

--- App/screens/HomeScreen.tsx
+++ App/screens/HomeScreen.tsx
@@ -3,12 +3,25 @@ import { Text, View } from '../components/Themed';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
-
+import ReactDOM from "react-dom";
+    import { BrowserRouter, Route, Switch } from "react-router-dom";
+ 
+    import Page1 from "./Page1";
+    import Page2 from "./Page2";
 export default function HomeScreen() {
   return ( 
-  <View style={styles.container}>
-    <Button title="Calendar" onPress={() => ('Calendar')} />
-  </View>
+    
+ 
+     const rootElement = document.getElementById("root");
+     ReactDOM.render(
+       <BrowserRouter>
+        <Switch>
+         <Route exact path="/" component={Page1} />
+         <Route path="/page2" component={Page2} />
+       </Switch>
+       </BrowserRouter>,
+       rootElement
+     );
 )}
 
 const styles = StyleSheet.create({
