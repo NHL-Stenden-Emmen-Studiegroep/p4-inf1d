a6f358abbb3126ed52aa8b66f85abdc53b98f8c0
DelanoKoster1 <delano.koster@student.nhlstenden.com>
2022-05-25 16:32:48 +0200

    made app version show on loading screen

--- App/screens/LoadingScreen.js
+++ App/screens/LoadingScreen.js
@@ -3,14 +3,15 @@ import { StyleSheet } from 'react-native';
 import {Logo} from "../components/Logo";
 import Colors from "../constants/Colors";
 import Background from "../components/Background";
-
+import {version as appVersion}  from '../package.json';
 
 export function LoadingScreen() {
     return (
     <View style={styles.container}>
         <Background style={styles.background} />
         <Logo width="150px" height="150px" />
-        <Text style={styles.text}>Loading...</Text>
+        <Text style={styles.LoadingText}>Loading...</Text>
+        <Text style={styles.version}>App version: {appVersion}</Text>
     </View>
     )
 }
@@ -33,8 +34,15 @@ const styles = StyleSheet.create({
     img: {
         height: 50,
     },
-    text: {
+    LoadingText: {
         color: Colors.light.textColorWhite,
         // fontFamily: 'montserrat',
     },
+
+    version: {
+        color: Colors.light.textColorBlack,
+        position: "absolute",
+        left: 10,
+        bottom: 10,
+    }
 })
