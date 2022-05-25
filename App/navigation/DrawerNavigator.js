505226cbcae6c892d9b1341cacb2356807cf1c55
Cedric Smit <cedsmit@gmail.com>
2022-05-25 13:44:28 +0200

    created a custom drawer component for icon

--- App/navigation/DrawerNavigator.js
+++ App/navigation/DrawerNavigator.js
@@ -1,4 +1,4 @@
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
 import { Text } from '../components/Themed';
 import HomeScreen from '../screens/HomeScreen';
 
@@ -14,13 +14,22 @@ const Drawer = createDrawerNavigator();
 
 export function DrawerNavigator() {
   return (
-    <Drawer.Navigator initialRouteName="Home">
+    <Drawer.Navigator initialRouteName="Home" drawerContent={(props) => <NavigationComponent {...props} />}>
       <Drawer.Screen name="Home" component={HomeScreen} />
       <Drawer.Screen name="Calendar" component={PlaceHolderComp} />
       <Drawer.Screen name="Notes" component={PlaceHolderComp} />
       <Drawer.Screen name="To-Do" component={PlaceHolderComp} />
       <Drawer.Screen name="Settings" component={PlaceHolderComp} />
     </Drawer.Navigator>
+  );
+}
+
+function NavigationComponent(props) {
+  return (
+    <DrawerContentScrollView {...props}>
+      <Text>This is a header</Text>
+      <DrawerItemList {...props} />
+    </DrawerContentScrollView>
   );
 }
 
