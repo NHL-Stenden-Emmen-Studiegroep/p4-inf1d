45e0ba11644bb2610e7e44950c5fb8a8f6eb8797
DelanoKoster1 <delano.koster@student.nhlstenden.com>
2022-05-25 15:36:59 +0200

    Removed unused code from Background.js

--- App/components/Background.js
+++ App/components/Background.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 import Svg, { Defs, ClipPath, Path, G, Circle } from "react-native-svg"
 
-function SvgComponent(props) {
+function Background(props) {
   return (
     <Svg
       xmlns="http://www.w3.org/2000/svg"
@@ -66,66 +66,4 @@ function SvgComponent(props) {
   )
 }
 
-export default SvgComponent
-
-// import Colors from "../constants/Colors";
-// import { StyleSheet } from 'react-native';
-// import { View } from "../components/Themed";
-
-
-
-
-// const Background = () => {
-//     return(
-//         <>
-//         <View style={[styles.circleBL, styles.circle]}></View>
-//         <View style={[styles.circleBR, styles.circle]}></View>
-//         <View style={[styles.circleTL, styles.circle]}></View>
-//         <View style={[styles.circleTR, styles.circle]}></View>
-//         </>
-//     );
-// }
-
-// const rad = 700;
-
-// const styles = StyleSheet.create({
-//     circle: {
-//         height: rad,
-//         width: rad,
-//         borderRadius: rad,
-//         position: "absolute",
-//     },
-
-//     circleTL: {
-//         backgroundColor: Colors.light.colorBlue700,
-//         top: -150,
-//         left: -300,
-//         zIndex: -5
-//     },
-
-//     circleTR: {
-//         backgroundColor: Colors.light.bgColorWhite,
-//         top: -300,
-//         right: -400,
-//         zIndex: -3,
-//     },
-
-//     circleBR: {
-//         backgroundColor: Colors.light.colorBlue500,
-//         bottom : -150,
-//         right: -350,
-//         zIndex: -4,
-//     },
-
-//     circleBL: {
-//         backgroundColor: Colors.light.bgColorWhite,
-//         bottom: -350,
-//         left: -450,
-//         zIndex: -3,
-//     }
-// })
-
-
-
-
-// export default Background;
+export default Background
