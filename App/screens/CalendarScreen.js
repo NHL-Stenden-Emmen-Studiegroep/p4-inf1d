89009d30a850f3f1421adb0c88ca401d24d97204
JaafarJawadi29 <91067539+JaafarJawadi29@users.noreply.github.com>
2022-05-25 13:44:56 +0200

    month page

--- App/screens/CalendarScreen.js
+++ App/screens/CalendarScreen.js
@@ -1,99 +1,55 @@
 import { StyleSheet } from 'react-native';
 import { Text, View } from '../components/Themed';
-import React, { Component } from 'react';
-import { month } from 'react-big-calendar/lib/utils/dates';
+import React, { Component, useState } from 'react';
+import { Calendar, CalendarList, LocaleConfig } from 'react-native-calendars';
 
-var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-
-function drawCalendarMonths(){
-    for(var i = 0; i < months.length; i++){
-        var doc = document.createElement("div");
-        doc.innerHTML = months[i];
-        doc.classList.add("dropdown-item");
-
-        doc.onclick = (function () {
-            var selectedMonth = i;
-            return function ()
-            {
-                month = selectedMonth;
-                document.getElementById("curMonth").innerHTML = months[month];
-                loadCalendarDays();
-                return month;
-            }
-        })();
-        document.getElementById("months").appendChild(doc);
-    }
-}
-
-function loadYears(){
-    var startYear = 1900;
-    var endYear = 2022;
-
-    for(var i = startYear; i <= endYear; i++){
-        var doc = document.createElement("div");
-        doc.innerHTML = i;
-        doc.classList.add("dropdown-item");
-
-        doc.onclick = (function(){
-            var selectedYear = i;
-            return function(){
-                year = selectedYear;
-                document.getElementById("curYear").innerHTML = year;
-                loadCalendarDays();
-                return year;
-            }
-        })();
-        document.getElementById("years").appendChild(doc);
-    }
-}
-
-function daysInMonth(month, year){
-    let d = new Date(year, month+1, 0);
-    return d.getDate();
-}
-
-function loadCalendarDays(){
-    document.getElementById("calendarDays").innerHTML = "";
-
-    var tmpDate = new Date(year, month, 0);
-    var num = daysInMonth(month, year);
-    var dayofweek = tmpDate.getDay();
-
-    for(var i = 0; i <= dayofweek; i++){
-        var d = document.createElement("div");
-        d.classList.add("day");
-        d.classList.add("blank");
-        document.getElementById("calendarDays").appendChild(d);
-    }
-
-    for(var i = 0; i < num; i++){
-        var tmp = i + 1;
-        var d = document.createElement("div");
-        d.id = "calendarday_" + tmp;
-        d.className = "day";
-        d.innerHTML = tmp;
-        document.getElementById("calendarDays").appendChild(d);
-    }
-
-    var clear = document.createElement("div");
-    clear.className = "clear";
-    document.getElementById("calendarDays").appendChild(clear);
-}
-
-var selectedDays = new Array();
-var mousedown = false;
+LocaleConfig.locales['en'] = {
+  monthNames: [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ],
+  monthNamesShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+  dayNames: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+  dayNamesShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+  today: 'Today'
+};
+LocaleConfig.defaultLocale = 'en';
 
 
 
+export default function CalendarScreen(){
+    const _date = new Date();
+    const [date, setDate] = useState({
+            day: _date.getDay(),
+            month: _date.getMonth(),
+            year: _date.getFullYear()
+    });
+
+    return(
+    <View style={styles.container}>
+    <Text style={styles.title}>Calendar</Text>
+    <Calendar/>
+    </View>
+    )
+}          
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: 'bold'
   },
   separator: {
     marginVertical: 30,
