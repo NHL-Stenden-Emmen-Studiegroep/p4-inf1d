d30333f4c833efe3b63bebabf7039cc336584703
Cedric Smit <cedsmit@gmail.com>
2022-06-08 11:43:33 +0200

    variable is now correctly being saved into storage. and is also correclty being read

--- App/hooks/useStorage.js
+++ App/hooks/useStorage.js
@@ -1,27 +1,20 @@
-import React, {useState, useEffect, useCallback} from "react";
-import { useAsyncStorage } from "@react-native-async-storage/async-storage";
+import { useState, useEffect, useCallback } from 'react';
+import { useAsyncStorage } from '@react-native-async-storage/async-storage';
 
 export function useStorage(storeName) {
-    let value, setValue;
+  let [value, setValue] = useState();
 
-    const {getItem, setItem} = useAsyncStorage(storeName);
+  const { getItem, setItem } = useAsyncStorage(storeName);
 
+  const readItemFromStorage = useCallback(async () => {
+    setValue(await getItem());
+    return await getItem();
+  }, []);
 
-    const readItemFromStorage = useCallback(async () => {
-        const item = await getItem();
-        value = item;
-        console.log("Storage is called")
-        return value;
-    }, []);
+  const writeItemToStorage = useCallback(async (newValue) => {
+    await setItem(newValue);
+    setValue(newValue);
+  }, []);
 
-    const writeItemToStorage = useCallback(async newValue => {
-        await setItem(newValue);
-        value = newValue;
-    }, []);
-
-    useEffect(() => {
-        readItemFromStorage();
-    }, [value])
-
-    return [value, writeItemToStorage];
+  return [readItemFromStorage, writeItemToStorage];
 }
--- App/navigation/index.js
+++ App/navigation/index.js
@@ -2,11 +2,11 @@ import * as React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { DarkTheme, DefaultTheme, NavigationContainer } from '@react-navigation/native';
 
-import NotFoundScreen from '../screens/NotFoundScreen';
 import { DrawerNavigator } from './DrawerNavigator';
 import { LoadingScreen } from '../screens/LoadingScreen';
 import { WelcomeScreen } from '../screens/WelcomeScreen';
 import { ConnectScreen } from '../screens/ConnectScreen';
+import { useStorage } from '../hooks/useStorage';
 
 export default function Navigation({ colorScheme }) {
   return (
@@ -20,12 +20,11 @@ const Stack = createNativeStackNavigator();
 
 export function RootNavigator() {
   return (
-    <Stack.Navigator initialRouteName="Root">
+    <Stack.Navigator initialRouteName="Loading">
       <Stack.Screen name="Loading" component={LoadingScreen} options={{ headerShown: false }} />
       <Stack.Screen name="Root" component={DrawerNavigator} options={{ headerShown: false }} />
       <Stack.Screen name="Welcome" component={WelcomeScreen} options={{ headerShown: false }} />
       <Stack.Screen name="Connect" component={ConnectScreen} options={{ headerShown: true }} />
-      <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
     </Stack.Navigator>
   );
 }
