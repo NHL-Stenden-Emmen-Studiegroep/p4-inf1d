7b4767538a2fe2e0fe1290dbe4cb785b15929ee9
Cedric Smit <cedsmit@gmail.com>
2022-06-08 13:09:03 +0200

    removed unused statement

--- App/hooks/useStorage.js
+++ App/hooks/useStorage.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useCallback } from 'react';
 import { useAsyncStorage } from '@react-native-async-storage/async-storage';
 
 export function useStorage(storeName) {
