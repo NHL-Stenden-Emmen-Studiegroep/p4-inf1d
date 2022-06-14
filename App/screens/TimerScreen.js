83319014aa6f1bc0dd9e3392ad59148a78ccc23a
DelanoKoster1 <delano.koster@student.nhlstenden.com>
2022-06-14 12:11:30 +0200

    made pomodoro timer

--- App/screens/TimerScreen.js
+++ App/screens/TimerScreen.js
@@ -1,61 +1,97 @@
 import { View, Text } from "../components/Themed";
 import { StyleSheet, Pressable } from "react-native";
 import Colors from '../constants/Colors';
+import { Timer } from 'react-native-stopwatch-timer';
 import { useState } from "react";
-import { Settings } from "react-native";
-import TimerText from "../components/TimerText";
+import { Ionicons } from '@expo/vector-icons';
 
 export function TimerScreen({navigation}) {
-    const [time, setTime] = useState(Settings['focus-time'] * 60);
+  const [seconds, setSeconds] = useState(5);
+  const [minutes, setMinutes] = useState(0);
+  const [hours, setHours] = useState(0);
+
+  const [isTimerStart, setisTimerStart] = useState(false);
+  const [resetTimer, setResetTimer] = useState(false);
+
+  const TimerDuration = (((hours * 60) + minutes) * 60 + seconds) * 1000;
 
     return (
-        <View style={Styles.OuterContainer}>
-          <View style={Styles.InnerContainer}>
-            <View style={Styles.TitleContainer}>
-              <Text style={Styles.SectionTitle}>Pomodoro Timer</Text>
-              <Pressable>
-                <Text style={Styles.SettingsButton}>Settings</Text>
-              </Pressable>
-            </View>
-            <View style={Styles.TimerContainer}>
-              <TimerText time={time} />
-              {/* <ControlButtons func={setTime} /> */}
-            </View>
-            <View>
-              <Text style={Styles.TasksText}>No tasks available yet.</Text>
-            </View>
-          </View>
+      <View style={Styles.container}>
+        <View style={Styles.Header}>
+          <Text style={Styles.SectionTitle}>Pomodoro Timer</Text>
+          <Pressable onPress={() => navigation.navigate('TimerSettings')}>
+            <Text style={Styles.SettingsButton}>Settings</Text>
+          </Pressable>
         </View>
+        <View style={Styles.TimerContainer}>
+          <Timer
+            totalDuration={TimerDuration}
+            start={isTimerStart}
+            reset={resetTimer}
+            handleFinish={() => {
+              alert('Timer klaar');
+            }}
+          />
+        </View>
+        <View style={Styles.TimerButtons}>
+          <Pressable style={Styles.button} onPress={() => setisTimerStart(true)}>
+            {!isTimerStart ? (
+              <Ionicons style={Styles.start} name='play-outline' size={50} />
+            ) : (
+              <Ionicons name='pause-outline' size={50} />
+            )}
+          </Pressable>
+          <Pressable style={Styles.button} onPress={() => setResetTimer(true)}>
+            <Ionicons name="refresh-outline" size={50} />
+          </Pressable>
+        </View>
+        <View>
+          <Text style={Styles.TasksText}>No tasks available yet.</Text>
+        </View>
+      </View>
     );
 }
 
 const Styles = StyleSheet.create({
-  OuterContainer: {
+  container: {
     alignItems: 'center',
   },
-  InnerContainer: {
-    width: '80%',
-    marginVertical: 20,
-    alignItems: 'center',
-  },
-  TitleContainer: {
+  Header: {
     width: '100%',
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
   },
   SectionTitle: {
+    paddingHorizontal: 50,
     color: Colors.light.textColorBlack,
     fontSize: 22,
     fontWeight: '700',
   },
   SettingsButton: {
+    paddingHorizontal: 60,
     color: Colors.light.colorBlue900,
     textDecorationLine: 'underline',
     textDecorationColor: Colors.light.colorBlue900,
   },
   TimerContainer: {
-    marginVertical: 20,
+    marginVertical: 30,
+  },
+  TimerButtons: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+  },
+  button: {
+    backgroundColor: Colors.light.colorBlue500,
+    width: 70,
+    height: 70,
+    borderRadius: 70,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  start: {
+    paddingLeft: 5,
   },
   TasksText: {
     color: Colors.light.colorGray800,
---
+++ App/screens/TimerSettingsScreen.js
@@ -0,0 +1,64 @@
+import { StyleSheet } from 'react-native';
+import { View, Text } from "../components/Themed"
+import Colors from '../constants/Colors';
+import Settings from '../assets/settings/PomodoroTimer.json';
+
+
+export function TimerSettingsScreen() {
+  const SaveButtonHandler = () => {};
+
+  return (
+    <View style={Styles.OuterContainer}>
+      <View style={Styles.InnerContainer}>
+        <View style={Styles.TitleContainer}>
+          <Text style={Styles.ScreenTitle}>Timer Settings</Text>
+        </View>
+        <View style={Styles.DropdownContainer}>
+          <Dropdown name="Focus Time" extra="min" value={Settings['focus-time']}></Dropdown>
+          <Dropdown name="Short Break" extra="min" value={Settings['short-breaks']}></Dropdown>
+          <Dropdown name="Long Break" extra="min" value={Settings['long-breaks']}></Dropdown>
+          <Dropdown name="Intervals" value={Settings.intervals}></Dropdown>
+        </View>
+        <View style={Styles.SaveOrCancel}>
+          <Button text="Cancel" />
+          <Save func={() => console.log(Settings)} />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const Styles = StyleSheet.create({
+  OuterContainer: {
+    position: 'absolute',
+    width: '100%',
+    height: '90%',
+    bottom: 0,
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  InnerContainer: {
+    width: '80%',
+    marginVertical: 20,
+    flex: 1,
+  },
+  ScreenTitle: {
+    color: Colors.light.textColorBlack,
+    fontSize: 22,
+    fontWeight: '700',
+  },
+  TitleContainer: {},
+  DropdownContainer: {
+    flex: 1,
+    marginVertical: 20,
+    maxHeight: '100%',
+    overflow: 'hidden',
+  },
+  SaveOrCancel: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+  },
+});
+
+
