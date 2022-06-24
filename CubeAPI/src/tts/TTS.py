6914451436c8f079ab6bc70852f4837f5da76b34
Nick <nick.buisman@student.nhlstenden.com>
2022-06-24 22:56:05 +0200

    Added boot file and fixed filenotfound error

--- CubeAPI/src/tts/TTS.py
+++ CubeAPI/src/tts/TTS.py
@@ -7,16 +7,22 @@ from src.log.log import Log
 class TTS:
     def __init__(self, text: str, lang: str = "nl") -> None:
         self.text = text
-        self.temp = "./src/tts/temp.mp3"
+        self.temp = "/home/pi/Desktop/CubeAPI/src/tts/temp.mp3"
         if platform.system() == "Windows":
             cmd = "start"
         else:
             cmd = "mpg321"
         self.cmd = f"{cmd} {self.temp}"
         self.gtts = gTTS(text=self.text, lang=lang, slow=False)
-        self.gtts.save("./src/tts/temp.mp3")
+        self.gtts.save("/home/pi/Desktop/CubeAPI/src/tts/temp.mp3")
         Log(f"Created TTS -> {self.text}")
+
+    def playboot(self) -> None:
+        cmd = "mpg321"
+        self.temp = "/home/pi/Desktop/CubeAPI/src/tts/setup.mp3"
+        self.cmd = f"{cmd} {self.temp}"
 
     def play(self):
         os.system(self.cmd)
         Log(f"Played TTS -> {self.text}")
+
